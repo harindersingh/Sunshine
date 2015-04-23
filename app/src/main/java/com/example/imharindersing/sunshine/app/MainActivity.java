c15efa459652710f68fa5389b19789dd2c40c954
HARINDER SINGH <im.harinder.singh@gmail.com>
2015-04-23 23:30:29 +0530

    setup URL connection and load real time
    openweathermapAPI registered
    logcat
    android app stopped working

--- app/src/main/java/com/example/imharindersing/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/imharindersing/sunshine/app/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.imharindersing.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -78,14 +83,13 @@ public class MainActivity extends ActionBarActivity {
                     "Sunday - Sunny - 80/61"
             };
 
-            List<String> weekForecast = new ArrayList<String>(
-                    Arrays.asList(forecastArray));
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
 
             //now that we have some dummy forecast data, create an ArrayAdapter.
             //the ArrayAdapter will take data from a source(like our dummy forecast)
             //and use it to populate the ListView it's attached to.
 
-            ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(
+            ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(
                     //The current context
                     getActivity(),
                     //ID of list item layout
@@ -96,10 +100,68 @@ public class MainActivity extends ActionBarActivity {
                     weekForecast
             );
 
-            ListView listView = (ListView) rootView.findViewById(
-                    R.id.listview_forecast);
-            listView.setAdapter(mForecastAdapter);
+            // Get a reference to the ListView, and attach this adapter to it.
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(forecastAdapter);
 
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?id=1277333&APPID=39656c4e32df1a3da57376e89a2bb099&mode=json&units=metric&cnt=7");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
             return rootView;
         }
     }
